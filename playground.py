94c0f26f3da001241ebeb2d0f9b290b0bb5a76e2
Afam <afamezechukwu@gmail.com>
2020-01-11 07:59:01 +1100

    Added a fruit to fruits

--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
 # Python 3: List comprehensions
-fruits = ['Banana', 'Apple', 'Lime']
+fruits = ['Banana', 'Apple', 'Lime', 'Orange']
 loud_fruits = [fruit.upper() for fruit in fruits]
 print(loud_fruits)
